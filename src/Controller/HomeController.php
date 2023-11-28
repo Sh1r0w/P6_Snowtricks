0f58690e7be32912489e3062abb3814a7c3d5c9b
jc <k4leth@gmail.com>
2023-11-28 23:21:10 +0100

    add findAll
    Add FindAll figure in home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Figure;
@@ -11,8 +12,12 @@ use App\Entity\Figure;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, ManagerRegistry $doctrine): Response
     {
+
+        $figure = $doctrine->getRepository(Figure::class);
+        $figures = $figure->findAll();
+        
         /*$figure = new Figure();
         $figure->setTitle(title: 'Test')
                 ->setDescription(description: 'Premier Test')
@@ -22,6 +27,7 @@ class HomeController extends AbstractController
         $entityManager->flush();*/
         return $this->render('home.twig', [
             'controller_name' => 'HomeController',
+            'figures' => $figures,
         ]);
     }
 }
