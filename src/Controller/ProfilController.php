7e94a26f010e9ce820c548b12d168b8400f30687
jc <k4leth@gmail.com>
2024-01-01 22:28:13 +0100

    refacto profil controller

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,46 +17,49 @@ use App\Services\ImgService;
 
 class ProfilController extends AbstractController
 {
+    private $user;
 
     public function __construct(
+        private Security $security,
         private ImgService $img, 
         private ManagerRegistry $doctrine,
         private UserPasswordHasherInterface $userPasswordHasher,
         private EntityManagerInterface $entityManager,
-        ){
+        ) {
 
+            
     }
 
 
     #[Route('/profil', name: 'app_profil')]
     public function index(Request $request): Response {
-
+        
+        if ($this->security->isGranted('ROLE_USER')){
+            $id = $this->getUser()->getId();
+            $this->user = $this->doctrine->getRepository(Connect::class)->find($id);
+            }
 
         if (!is_null($this->getUser())){
 
-        $id = $this->getUser()->getId();
-        $user = $this->doctrine->getRepository(Connect::class)->find($id);
-
-        $profilForm = $this->createForm(profilType::class, $user);
-        $passForm = $this->createForm(PassFormType::class, $user);
+        $profilForm = $this->createForm(profilType::class, $this->user);
+        $passForm = $this->createForm(PassFormType::class, $this->user);
 
         $profilForm->handleRequest($request);
         $passForm->handleRequest($request);
-
-        $image = $profilForm->get('imguser')->getData();
+        
         if ($profilForm->isSubmitted() && $profilForm->isValid()) {
-            if($image != null) {
-                $folder = $user->getUsername();
-                $recordImg = $this->img->addImg($image, $folder);
-                $user->setImguser($recordImg);
+            if($profilForm->get('imguser')->getData() != false) {
+                $image = $profilForm->get('imguser')->getData();
+                $recordImg = $this->img->addImg($image, $this->user->getUsername());
+                $this->user->setImguser($recordImg);
             };    
             $this->addFlash('success', 'Profil Modifié');      
         }
         
         if($passForm->isSubmitted() && $passForm->isValid()){
-            $user->setPassword(
+            $this->user->setPassword(
                 $this->userPasswordHasher->hashPassword(
-                    $user,
+                    $this->user,
                     $passForm->get('password')->getData()
                 )
                
@@ -64,7 +68,7 @@ class ProfilController extends AbstractController
             $this->addFlash('success','Mot de passe modifié');
         }
 
-        $this->entityManager->persist($user);
+        $this->entityManager->persist($this->user);
         $this->entityManager->flush();
         
         return $this->render('profil/index.html.twig', [
@@ -77,22 +81,5 @@ class ProfilController extends AbstractController
      }
     }
 
-    #[Route(path: 'deleteImgProfil/{id}', name: 'delete_img')]
-    public function deleteImg(): Response {
-
-        $this->entityManager->remove($image);
-        $this->entityManager->flush();
-
-        $success = $this->img->deleteOne($image, $figure->getTitle());
-
-        if ($success) {
-            $this->addFlash('success', 'Image Supprimée avec succès');
-        } else {
-            $this->addFlash('error', 'Echec de la suppresion');
-        }
-
-        return $this->redirectToRoute('update_figure', ['figure' => $figure->getId()]);
-
-    }
-
+    
 }
