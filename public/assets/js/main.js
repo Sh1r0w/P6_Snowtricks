5f0032424f915739a62cc314d92dbf2d4e01dc12
jc <k4leth@gmail.com>
2024-01-28 15:55:59 +0100

    Update Front
    Update Figure Page
    Update Page
    Profile Page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,16 +1,15 @@
+
 window.addEventListener('scroll', (event) => {
     let scrollY = this.scrollY;
     let icon = document.querySelector('#iconDown')
     let iconUp = document.querySelector('#iconUp')
 
-    if (scrollY >= 1) {
+    if (scrollY >= 1 && document.querySelector('.home_banner')) {
         icon.style.display = 'none'
         iconUp.style.display ='block'
-    } else if (scrollY === 0) {
+    } else if (scrollY === 0 && document.querySelector('.home_banner')) {
         icon.style.display = 'block'
         iconUp.style.display = 'none'
     }
-
-    
 
   });
